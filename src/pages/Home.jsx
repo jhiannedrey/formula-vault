ff9212fe7ee2253501f49ccbaf154f10cd5fcf11
jhiannedrey <jhiannedrey12@gmail.com>
2026-02-17 22:31:22 +0800

    Delete src directory

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding: "20px"}}>
-      <h1>Formula Vault Pro</h1>
-      <p>Search and manage Excel, Google Sheets, and VBA formulas.</p>
-    </div>
-  );
-}
